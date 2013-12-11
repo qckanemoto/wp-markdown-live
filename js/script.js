c4408a295890e9bd39e94acf802404d917e5a482
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2013-12-11 11:26:01 +0900

    記事編集画面のレイアウトを段組に

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 $(function() {
-	$("#wp-content-editor-container").before("<div style='float:right; margin-right:1em;'><p><a href='http://daringfireball.net/projects/markdown/syntax' target='_blank'>Markdown syntax ?</a></p></div>");
+    // insert syntax link.
+    setTimeout(function() {
+        $("#ed_toolbar").append("<div style='float:right; margin-top:-0.6em;'><p><a href='http://daringfireball.net/projects/markdown/syntax' target='_blank'>Markdown syntax ?</a></p></div>");
+    }, 1000);
 
-	$("#postdivrich").after("<div id='wp-markdown-live-preview'></div>");
+    // resize textarea.
+    var half = $("#postdivrich").width() / 2 - 5;
+    $("#postdivrich").width(half).css("float", "left");
 
-	new Behave({
-		textarea: document.getElementById('content'),
-		replaceTab: true,
-		softTabs: true,
-		tabSize: 4,
-		autoOpen: true,
-		overwrite: true,
-		autoStrip: true,
-		autoIndent: true,
-		fence: false
-	});
+    // insert preview area.
+    var $preview = $("<div id='wp-markdown-live-preview'></div>").width(half).css("float", "right").css("margin-top", $("#wp-content-editor-tools").height());
+    $("#postdivrich").after($preview);
 
-	$("textarea#content").on("keyup change", function(e) {
-		$("#wp-markdown-live-preview").html("<p>" + marked($(this).val()) + "</p>");
-	}).change();
+    // customize textarea.
+    new Behave({
+        textarea: document.getElementById('content'),
+        replaceTab: true,
+        softTabs: true,
+        tabSize: 4,
+        autoOpen: true,
+        overwrite: true,
+        autoStrip: true,
+        autoIndent: true,
+        fence: false
+    });
+
+    // handle event on textarea.
+    $("textarea#content").on("keyup change", function(e) {
+        $("#wp-markdown-live-preview").html("<p>" + marked($(this).val()) + "</p>");
+    }).change();
 });

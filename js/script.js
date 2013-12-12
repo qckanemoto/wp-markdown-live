33d191534dbc75253c32380e8cb3cc41325bc5bb
root <root@www15236uf.sakura.ne.jp>
2013-12-12 13:06:48 +0900

    レイアウト調整のタイミングを修正

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 $(function() {
-    // insert syntax link.
+    // delay re-layout.
     setTimeout(function() {
+        // insert syntax link.
         $("#ed_toolbar").append("<div style='float:right; margin-top:-0.6em;'><p><a href='http://daringfireball.net/projects/markdown/syntax' target='_blank'>Markdown syntax ?</a></p></div>");
+
+        // resize textarea.
+        var half = $("#postdivrich").width() / 2 - 5;
+        $("#postdivrich").width(half).css("float", "left");
+
+        // insert preview area.
+        var $preview = $("<div id='wp-markdown-live-preview'></div>").width(half).css("float", "right").css("margin-top", $("#wp-content-editor-tools").height());
+        $("#postdivrich").after($preview);
     }, 1000);
-
-    // resize textarea.
-    var half = $("#postdivrich").width() / 2 - 5;
-    $("#postdivrich").width(half).css("float", "left");
-
-    // insert preview area.
-    var $preview = $("<div id='wp-markdown-live-preview'></div>").width(half).css("float", "right").css("margin-top", $("#wp-content-editor-tools").height());
-    $("#postdivrich").after($preview);
 
     // customize textarea.
     new Behave({
